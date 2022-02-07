2e7abab54fb77aef8a0ebb665cabb02b859eb117
Marcos Henrique <marcosmarcone48@gmail.com>
2022-02-07 10:53:06 -0300

    Adicionado o " Jogar Novamente"
    Foi adicionada a função perguntando ao final do jogo se o usuário deseja jogar novamente. Caso, sim, então o jogo é reiniciado.
    
    Também foram feitos alguns ajustes na documentação do código.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
 
 /* Variáveis Globais */
+
+// Controlam as quantidades de cartas, acertos e jogadas
 let quantidadeDeCartas = 0;
 let quantidadeTotalDeAcertos = 0;
 let quantidadeAcertos = 0;
 let quantidadeDeJogadas = 0;
 
+// Tipos de cartas de acordo com os gifs
 let tiposDeCartas = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
 let tipoDeCartasSelecionadas = [];
 let cartas = [];
+
 /* Cada carta criada é um objeto que contém o seu tipo 
    e uma flag indicando se ela já foi selecionada */
 
-/* Controle do Jogo */
+// Controle do Jogo 
 let primeiraJogada = true;
 let primeiraCartaSelecionada = null;
 let divPrimeiraCarta = null;
 let segundaCartaSelecionada = null;
 let divSegundaCarta = null
 
+// Flag que indica se o jogo está habilitado para jogar
 let jogoHabilitado = true;
 
 // Flag que indica o primeiro clique do jogo para iniciar o relógio
@@ -33,122 +38,7 @@ let intervaloRelogio = null;
 
 
 
-function selecionarCarta(divCarta, indiceCarta) {
-    // Informações da carta
-    indiceCarta = parseInt(indiceCarta);
-
-
-    console.log(`Indice da Carta: ${indiceCarta}`)
-    console.log(`Tipo da Carta: ${cartas[indiceCarta].tipo}`)
-    console.log(`Carta Selecionada: ${cartas[indiceCarta].selecionada}`)
-
-    // Verifica se é o primeiro clique para habilitar o relogio
-    if (primeiroClique) {
-        intervaloRelogio = setInterval(atualizarRelogio, 1000);
-        primeiroClique = false;
-    }
-    
-    
-    // Verifica se a carta já está virada 
-    if (!cartas[indiceCarta].selecionada && jogoHabilitado) {
-
-        // Marca a carta como selecionada
-        cartas[indiceCarta].selecionada = true;
-
-        // Vira a carta
-        virarCarta(divCarta);
-
-        // Verifica se é a primeira carta do turno
-        if (primeiraJogada) {
-
-            console.log("Primeira Jogada!");
-
-            // Seleciona a carta
-            primeiraCartaSelecionada = cartas[indiceCarta];
-            divPrimeiraCarta = divCarta;
-
-            // Indica que não é a primeira carta (vamos para a segunda)
-            primeiraJogada = false;
-        }
-        else {
-
-            console.log("Segunda Jogada!");
-            // Seleciona a carta
-            segundaCartaSelecionada = cartas[indiceCarta];
-            divSegundaCarta = divCarta;
-
-            // Caso as cartas sejam do mesmo um acerto é contabilizado
-            if (primeiraCartaSelecionada.tipo === segundaCartaSelecionada.tipo) {
-                quantidadeAcertos++;
-                if (quantidadeAcertos == quantidadeTotalDeAcertos)
-                    setTimeout(encerrarJogo, 1000);
-
-                console.log("Acerto: " + quantidadeAcertos);
-            }
-
-            else {
-                setTimeout(desvirarCartas, 1000);
-
-                // Desabilita as jogadas enquanto espera as cartas serem desviradas
-                jogoHabilitado = false;
-
-                console.log("Errou o par!!");
-            }
-
-            // Reinicia para a primeira jogado do turno
-            primeiraJogada = true;
-        }
-
-        // Incrementa a quantidade de jogadas
-        quantidadeDeJogadas++;
-    }
-    else {
-        console.log("Erro ao jogar!!");
-    }
-
-}
-
-
-// Desvira as cartas caso forem encontradas cartas de diferentes tipos
-function desvirarCartas() {
-    // Desvirar primeira carta
-    desvirarCarta(divPrimeiraCarta);
-
-    // Desvirar segunda carta
-    desvirarCarta(divSegundaCarta);
-
-    // Habilita as cartas para serem selecionadas
-    primeiraCartaSelecionada.selecionada = false;
-    segundaCartaSelecionada.selecionada = false;
-
-    // Habilita o jogo
-    jogoHabilitado = true;
-}
-
-
-// Vira uma carta
-function virarCarta(divCarta) {
-    // Classes para virar a carta
-    const verso = divCarta.querySelector(".parte-traseira");
-    const frente = divCarta.querySelector(".parte-frontal");
-
-    // Vira a carta
-    verso.classList.add("rotacionar-verso");
-    frente.classList.add("rotacionar-frente");
-}
-
-// Desvira uma carta
-function desvirarCarta(divCarta) {
-
-    // Classes para desvirar a carta
-    const verso = divCarta.querySelector(".parte-traseira");
-    const frente = divCarta.querySelector(".parte-frontal");
-
-    // Desvira a carta
-    verso.classList.remove("rotacionar-verso");
-    frente.classList.remove("rotacionar-frente");
-}
-
+/* Inicializa o jogo */
 function iniciarJogo() {
 
     // Pergunta ao usuário a quantidade de cartas
@@ -156,16 +46,29 @@ function iniciarJogo() {
 
     // Valida se é um número par entre 4 e 14
     while ((quantidadeDeCartas < 4 || quantidadeDeCartas > 14) || (quantidadeDeCartas % 2 !== 0)) {
-        quantidadeDeCartas = parseInt(prompt(" Resposta Inválida! Tente Novamente!\nQuantas cartas você deseja? (números pares de 4 a 14)"));
+        quantidadeDeCartas = parseInt(prompt("Resposta Inválida! Tente Novamente!\nQuantas cartas você deseja? (números pares de 4 a 14)"));
     }
 
     // Quantidade total de acertos
     quantidadeTotalDeAcertos = quantidadeDeCartas / 2;
 
+    // Zera o relógio
+    zerarRelogio();
+
     // Cria as cartas
     criarCartas();
 }
 
+/* Reseta o relógio presente na página */
+function zerarRelogio() {
+
+    // Zerando o HTML do relógio
+    divRelogio.innerHTML = 0;
+    intervaloRelogio = null;
+}
+
+
+/* Seleciona e cria as cartas de maneira aleatória */
 function criarCartas() {
 
     //Embaralha os tipos de cartas
@@ -194,8 +97,13 @@ function criarCartas() {
 
 }
 
-// Monta as cartas no HTML
+
+/* Monta as cartas no HTML */
 function montarCartasNoHTML() {
+
+    // Zerando o HTML
+    divJogoDeCartas.innerHTML = "";
+
     for (let i = 0; i < cartas.length; i++) {
         let carta = cartas[i];
         divJogoDeCartas.innerHTML += `
@@ -210,31 +118,188 @@ function montarCartasNoHTML() {
     }
 }
 
-// Encerra o jogo quando todos os pares forem encontrados
+
+/* Embaralhas as cartas */
+function embaralharCartas(listaDeCartas) {
+    listaDeCartas.sort(comparador);
+}
+
+
+/* Função correspondente ao clique em uma carta .
+   Responsável por implementar toda a lógica do jogo. 
+*/
+function selecionarCarta(divCarta, indiceCarta) {
+
+    // Informações da carta
+    indiceCarta = parseInt(indiceCarta);
+
+    // Verifica se é o primeiro clique para habilitar o relogio
+    if (primeiroClique) {
+        intervaloRelogio = setInterval(atualizarRelogio, 1000);
+        primeiroClique = false;
+    }
+
+
+    // Verifica se a carta já está virada 
+    if (!cartas[indiceCarta].selecionada && jogoHabilitado) {
+
+        // Marca a carta como selecionada
+        cartas[indiceCarta].selecionada = true;
+
+        // Vira a carta
+        virarCarta(divCarta);
+
+        // Verifica se é a primeira carta do turno
+        if (primeiraJogada) {
+
+            // Seleciona a carta
+            primeiraCartaSelecionada = cartas[indiceCarta];
+            divPrimeiraCarta = divCarta;
+
+            // Indica que não é a primeira carta (vamos para a segunda)
+            primeiraJogada = false;
+        }
+        else {
+
+            // Seleciona a carta
+            segundaCartaSelecionada = cartas[indiceCarta];
+            divSegundaCarta = divCarta;
+
+            // Caso as cartas sejam do mesmo um acerto é contabilizado
+            if (primeiraCartaSelecionada.tipo === segundaCartaSelecionada.tipo) {
+                quantidadeAcertos++;
+                if (quantidadeAcertos == quantidadeTotalDeAcertos)
+                    setTimeout(encerrarJogo, 1000);
+            }
+
+            else {
+                setTimeout(desvirarCartas, 1000);
+
+                // Desabilita as jogadas enquanto espera as cartas serem desviradas
+                jogoHabilitado = false;
+            }
+
+            // Reinicia para a primeira jogado do turno
+            primeiraJogada = true;
+        }
+
+        // Incrementa a quantidade de jogadas
+        quantidadeDeJogadas++;
+    }
+}
+
+
+/* Vira uma carta */
+function virarCarta(divCarta) {
+
+    // Classes para virar a carta
+    const verso = divCarta.querySelector(".parte-traseira");
+    const frente = divCarta.querySelector(".parte-frontal");
+
+    // Vira a carta
+    verso.classList.add("rotacionar-verso");
+    frente.classList.add("rotacionar-frente");
+}
+
+
+/* Desvira uma carta */
+function desvirarCarta(divCarta) {
+
+    // Classes para desvirar a carta
+    const verso = divCarta.querySelector(".parte-traseira");
+    const frente = divCarta.querySelector(".parte-frontal");
+
+    // Desvira a carta
+    verso.classList.remove("rotacionar-verso");
+    frente.classList.remove("rotacionar-frente");
+}
+
+
+/* Desvira as cartas caso forem encontradas cartas de diferentes tipos */
+function desvirarCartas() {
+
+    // Desvirar primeira carta
+    desvirarCarta(divPrimeiraCarta);
+
+    // Desvirar segunda carta
+    desvirarCarta(divSegundaCarta);
+
+    // Habilita as cartas para serem selecionadas
+    primeiraCartaSelecionada.selecionada = false;
+    segundaCartaSelecionada.selecionada = false;
+
+    // Habilita o jogo
+    jogoHabilitado = true;
+}
+
+
+/* Encerra o jogo quando todos os pares forem encontrados */
 function encerrarJogo() {
+
     // Encerra o relogio
     clearInterval(intervaloRelogio);
 
     // Obtem o tempo de jogo
     const tempoJogado = parseInt(divRelogio.innerHTML);
-    
+
     alert(`Você ganhou em ${quantidadeDeJogadas} jogadas e em ${tempoJogado} segundos!`);
+
+    reiniciarJogo();
+
 }
 
-// Atualiza o relógio
-function atualizarRelogio(){
+
+/* Pergunta se o usuário deseja reiniciar o jogo */
+function reiniciarJogo() {
+
+    let reiniciar = prompt("Deseja jogar novamente? (s ou n)");
+
+    while (reiniciar !== "s" && reiniciar !== "n") {
+        reiniciar = prompt("Resposta Inválida!\nDeseja jogar novamente? (s ou n)");
+    }
+
+    if (reiniciar === "s") {
+        resetarVariaveis();
+        iniciarJogo();
+    }
+
+}
+
+/* Atualiza o relógio */
+function atualizarRelogio() {
     divRelogio.innerHTML = parseInt(divRelogio.innerHTML) + 1;
 }
 
-// Embaralhas as cartas
-function embaralharCartas(listaDeCartas) {
-    listaDeCartas.sort(comparador);
-}
 
-// Retorna um número aleatório
+/* Retorna um número aleatório */
 function comparador() {
     return Math.random() - 0.5;
 }
+
+
+/* Reseta todas as variáveis do jogo */
+function resetarVariaveis() {
+
+    quantidadeDeCartas = 0;
+    quantidadeTotalDeAcertos = 0;
+    quantidadeAcertos = 0;
+    quantidadeDeJogadas = 0;
+
+    tiposDeCartas = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+    tipoDeCartasSelecionadas = [];
+    cartas = [];
+
+    primeiraJogada = true;
+    primeiraCartaSelecionada = null;
+    divPrimeiraCarta = null;
+    segundaCartaSelecionada = null;
+    divSegundaCarta = null
+
+    jogoHabilitado = true;
+
+    primeiroClique = true;
+}
+
 
 
 /* Chamada da função iniciarJogo */
